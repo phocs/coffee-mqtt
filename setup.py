4e709e0acd5169c2ff7a467fd2a46132aa36a5ce
Alessio Volpe <alessio.volpe@modis.com>
2020-09-26 12:59:52 +0200

    Package setup 0.0.1

---
+++ setup.py
@@ -0,0 +1,33 @@
+import sys
+import setuptools
+
+sys.path.insert(0, 'src')
+from coffee.mqtt import __version__
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='coffee-mqtt',
+    version=__version__,
+    description="Python MQTT framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Alessio Volpe",
+    author_email="alessio.volpe@aol.com",
+    url="https://github.com/phocs/coffee-mqtt",
+    packages=setuptools.find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    license='MIT License Copyright (c) 2020 Alessio Volpe',
+    keywords='coffee',
+    classifiers=[
+        'License :: OSI Approved',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        'Topic :: Framework',
+        'Topic :: Mqtt'
+    ],
+    python_requires='>=3.6',
+)
